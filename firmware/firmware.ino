b011b20908d86e3ceef2149947f4856a675fac4c
Matthew Gates <matthewg42@gmail.com>
2016-11-07 18:48:31 +0000

    test buttons and Players

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include "Players.h"
 #include "Config.h"
+#include "ModeButton.h"
+#include "ResetButton.h"
 
 void setup()
 {
@@ -8,14 +10,22 @@ void setup()
     for (uint8_t i=0; i<PLAYER_COUNT; i++) {
         Players[i].begin();
     }
+    ModeButton.begin();
+    ResetButton.begin();
     delay(500);
     Serial.println(F("E:setup"));
 }
 
 void loop()
 {
+    // Give everything a timeslice
+    ModeButton.update();
+    ResetButton.update();
     for (uint8_t i=0; i<PLAYER_COUNT; i++) {
         Players[i].update();
+    }
+
+    for (uint8_t i=0; i<PLAYER_COUNT; i++) {
         Serial.print(i+1);
         Serial.print(F("UP vin="));
         Serial.print(Players[i].getVoltage());
@@ -24,6 +34,13 @@ void loop()
         Serial.print(F(", pwm="));
         Serial.print(Players[i].getPwm());
         Serial.print(F(";  "));
+
+        if (ModeButton.isPressed()) {
+            Serial.print(F(" ModeButton=1"));
+        }
+        if (ResetButton.isPressed()) {
+            Serial.print(F(" ResetButton=1"));
+        }
     }
     Serial.println(F(""));
 }
