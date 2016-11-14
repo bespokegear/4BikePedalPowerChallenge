62256fac2a644a2e4ef1f7314770ae3b01757370
Matthew Gates <matthewg42@gmail.com>
2016-11-14 14:54:18 +0000

    use row code for wait wiper

--- firmware/WaitMode.cpp
+++ firmware/WaitMode.cpp
@@ -30,7 +30,7 @@ void _WaitMode::start()
 #endif
     // Clean button presses from other modes
     ClockDisplay.display("Hi!");
-    _wipe = playerLedCount();
+    _wipe = playerDisplayRowCount();
     _last = millis();
 }
 
@@ -39,16 +39,23 @@ void _WaitMode::stop()
 #ifdef DEBUG
     Serial.println(F("WaitMode::stop()"));
 #endif
+    // in case we exit wait mode before the displays are wiped
+    // just clear them
+    for (uint8_t p=0; p<PLAYER_COUNT; p++) {
+        Players[p].LED().clear();
+        Players[p].LED().show();
+    }
+    
 }
 
 void _WaitMode::modeUpdate()
 {
     if (millis() - _last > 5) {
         ClockDisplay.redisplay();
-        if (_wipe > 0) {
+        if (_wipe >= 0) {
             _wipe--;
             for (uint8_t p=0; p<PLAYER_COUNT; p++) {
-                Players[p].LED().setPixelColor(_wipe, 0x000000UL);
+                Players[p].setRowColor(_wipe, 0x000000UL);
                 Players[p].showLED();
             }
         }
